361057ebebdeb16fbcb871a38e1f89313535d0cc
gkampitakis <gkabitakis@gmail.com>
2020-09-05 11:43:30 +0100

    add coverage report on tests and cover 100%

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -1,7 +1,6 @@
 package arrays
 
 func ReverseArrayString(input []string, modify bool) (reverse []string) {
-
 	if modify {
 		reverse = input
 	} else {
@@ -9,30 +8,28 @@ func ReverseArrayString(input []string, modify bool) (reverse []string) {
 		copy(reverse, input)
 	}
 
-	for i, j := 0, len(input)-1; i < len(input)/2; i, j = i+1, j+1 {
+	for i, j := 0, len(input)-1; i < len(input)/2; i, j = i+1, j-1 {
 
 		reverse[i], reverse[j] = reverse[j], reverse[i]
 
 	}
 
 	return
-
 }
 
 func ReverseArrayInt(input []int, modify bool) (reverse []int) {
-
 	if modify {
 		reverse = input
 	} else {
 		reverse = make([]int, len(input))
+		copy(reverse, input)
 	}
 
-	for i, j := 0, len(input)-1; i < len(input)/2; i, j = i+1, j+1 {
+	for i, j := 0, len(input)-1; i < len(input)/2; i, j = i+1, j-1 {
 
 		reverse[i], reverse[j] = reverse[j], reverse[i]
 
 	}
 
 	return
-
 }
--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -6,8 +6,8 @@ import (
 )
 
 func TestReverseArrayString(t *testing.T) {
-	testValues := [][]string{[]string{"Hello World", "Test", "Blue"}, []string{"1", "2", "3"}}
-	wantValues := [][]string{[]string{"Blue", "Test", "Hello World"}, []string{"3", "2", "1"}}
+	testValues := [][]string{[]string{"Hello World", "Test", "Blue", "Red"}, []string{"1", "2", "3"}}
+	wantValues := [][]string{[]string{"Red", "Blue", "Test", "Hello World"}, []string{"3", "2", "1"}}
 
 	t.Run("Reverse Arrays", func(t *testing.T) {
 		for i, test := range testValues {
@@ -46,6 +46,57 @@ func TestReverseArrayString(t *testing.T) {
 
 		for i, test := range testValues {
 			if got := ReverseArrayString(test, false); !reflect.DeepEqual(got, wantValues[i]) {
+				t.Errorf("Wanted %v but got %v", wantValues[i], got)
+			}
+
+			if reflect.DeepEqual(testValues[i], wantValues[i]) {
+				t.Errorf("Original value was modified")
+			}
+		}
+	})
+}
+
+func TestReverseArrayInt(t *testing.T) {
+	testValues := [][]int{[]int{1, 2, 3, 4}, []int{10, 20, 90, 200}}
+	wantValues := [][]int{[]int{4, 3, 2, 1}, []int{200, 90, 20, 10}}
+
+	t.Run("Reverse Arrays", func(t *testing.T) {
+		for i, test := range testValues {
+			if got := ReverseArrayInt(test, true); !reflect.DeepEqual(got, wantValues[i]) {
+				t.Errorf("Wanted %v but got %v", wantValues[i], got)
+			}
+		}
+	})
+
+	t.Run("Empty array", func(t *testing.T) {
+		got := ReverseArrayInt([]int{}, false)
+
+		if len(got) > 0 {
+			t.Errorf("Wanted [] but got %v", got)
+		}
+	})
+
+	t.Run("Modify true", func(t *testing.T) {
+		testValues := [][]int{[]int{1, 2, 3, 4}}
+		wantValues := [][]int{[]int{4, 3, 2, 1}}
+
+		for i, test := range testValues {
+			if got := ReverseArrayInt(test, true); !reflect.DeepEqual(got, wantValues[i]) {
+				t.Errorf("Wanted %v but got %v", wantValues[i], got)
+			}
+
+			if !reflect.DeepEqual(testValues[i], wantValues[i]) {
+				t.Errorf("Original value was not modified")
+			}
+		}
+	})
+
+	t.Run("Modify false", func(t *testing.T) {
+		testValues := [][]int{[]int{1, 2, 3, 4}}
+		wantValues := [][]int{[]int{4, 3, 2, 1}}
+
+		for i, test := range testValues {
+			if got := ReverseArrayInt(test, false); !reflect.DeepEqual(got, wantValues[i]) {
 				t.Errorf("Wanted %v but got %v", wantValues[i], got)
 			}
 
